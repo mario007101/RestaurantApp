f705bf8c3bcc14d4ce55ac7af9141c43eab61ba2
mario007101 <gtamarkovic@gmail.com>
2023-07-08 22:11:21 +0200

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Mário Markovič on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
