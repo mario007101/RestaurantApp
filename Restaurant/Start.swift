66aef1440ef2b713eb1d8f7709d3d809a530f9b4
mario007101 <gtamarkovic@gmail.com>
2023-07-11 18:18:24 +0200

    added animated images

--- Restaurant/Start.swift
+++ Restaurant/Start.swift
@@ -1,27 +1,42 @@
 import SwiftUI
 
 struct Start: View {
+    let images = ["aboutUs", "podKastielomLogo"]
+    public let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    @State private var selection = 0
+    
     var body: some View {
         VStack(alignment: .center) {
-            ScrollView {
+                
                 Text("O Nás\n")
                     .font(.largeTitle)
                     .bold()
                 
-                Image("aboutUs")
-                    .resizable()
-                    .cornerRadius(20)
-                    .frame(width: 200, height: 200)
-                    .padding(.top, -50)
+            VStack(alignment: .center) {
+                    TabView(selection: $selection){
+                        ForEach(0..<2){ i in
+                            Image("\(images[i])")
+                                .resizable()
+                                .frame(width: 200,height: 200)
+                                .ignoresSafeArea()
+                                .cornerRadius(20)
+                        }
+                    }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        .onReceive(timer, perform: { _ in
+                            withAnimation{
+                                selection = selection < 3 ? selection + 1 : 0
+                            }
+                        })
+                }.padding(.top, -570)
                 
                 Text("Naša reštaurácia sa nachádza v dedinke Rumanová, neďaleko Nitry. Okolie kaštieľa prepožičiava miestu rustikálny vzhľad, a reštaurácia tak ponúka svojim stravníkom historickú atmosféru.")
                     .multilineTextAlignment(.center)
                     .frame(width: 300)
                     .font(.title2)
-                    .padding(.top, 10)
+                    .padding(.top, -450)
                 
                 Spacer()
-            }            
+            
         }.background(Color.clear)
     }
 }
