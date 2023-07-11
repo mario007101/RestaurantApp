fb7f5de8929c218f220f7aa43c54704f1776ca9f
mario007101 <gtamarkovic@gmail.com>
2023-07-11 17:48:43 +0200

    added photo

--- Restaurant/Start.swift
+++ Restaurant/Start.swift
@@ -3,18 +3,25 @@ import SwiftUI
 struct Start: View {
     var body: some View {
         VStack(alignment: .center) {
-            Text("O Nás\n")
-                .font(.largeTitle)
-                .bold()
-            
-            Text("Naša reštaurácia sa nachádza v dedinke Rumanová, neďaleko Nitry. Okolie kaštieľa prepožičiava miestu rustikálny vzhľad, a reštaurácia tak ponúka svojim stravníkom historickú atmosféru.")
-                .multilineTextAlignment(.center)
-                .frame(width: 300)
-                .font(.title2)
-                .padding(.top, -50)
-            
-            Spacer()
-            
+            ScrollView {
+                Text("O Nás\n")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Image("aboutUs")
+                    .resizable()
+                    .cornerRadius(20)
+                    .frame(width: 200, height: 200)
+                    .padding(.top, -50)
+                
+                Text("Naša reštaurácia sa nachádza v dedinke Rumanová, neďaleko Nitry. Okolie kaštieľa prepožičiava miestu rustikálny vzhľad, a reštaurácia tak ponúka svojim stravníkom historickú atmosféru.")
+                    .multilineTextAlignment(.center)
+                    .frame(width: 300)
+                    .font(.title2)
+                    .padding(.top, 10)
+                
+                Spacer()
+            }            
         }.background(Color.clear)
     }
 }
