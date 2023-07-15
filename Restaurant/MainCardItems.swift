e57364297a34e7f2871a23020e00f50a066dba30
mario007101 <gtamarkovic@gmail.com>
2023-07-15 16:16:49 +0200

    added ingredients list

--- Restaurant/MainCardItems.swift
+++ Restaurant/MainCardItems.swift
@@ -1,17 +1,19 @@
 import SwiftUI
 
-struct Items: Identifiable {
+struct Items: Identifiable, Equatable {
     let id = UUID()
     let name: String
     let gram: Int
     let price: Double
+    let ingredients: [String]
 }
 
 struct MainCardItems: View {
     let allPizzas: [Items] = [.pizzaStick, .margharita, .sunkova, .funghi, .cardinala, .ungarese]
-    
-    let topPizzas //Have to finish
-    
+    let topPizzas: [Items] = [] // Have to finish
+
+    @State private var selectedPizza: Items? = nil
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -20,11 +22,31 @@ struct MainCardItems: View {
                         HStack {
                             Text("\(item.name) (\(item.gram)g)")
                             Spacer()
-                            Text("\(item.price, specifier: "%.2f") €")
+                            Text("\(item.price, specifier: "%.2f") €").foregroundColor(CustomColors.typicalColor)
+
+                            Button(action: {
+                                withAnimation {
+                                    selectedPizza = (selectedPizza == item) ? nil : item
+                                }
+                            }) {
+                                Image(systemName: selectedPizza == item ? "chevron.down" : "chevron.right")
+                                    .foregroundColor(.white)
+                            }
                         }
                         .padding(.horizontal)
                         .bold()
                         .font(.title2)
+                       
+
+                        if selectedPizza == item {
+                            ForEach(item.ingredients, id: \.self) { ingredient in
+                                
+                                Text(ingredient)
+                                    .foregroundColor(CustomColors.typicalColor)
+                                    .padding(.leading, -140)
+                                    .bold()
+                            }
+                        }
                         Divider()
                     }
                 }
@@ -34,6 +56,7 @@ struct MainCardItems: View {
             CustomColors.navigationColor
                 .ignoresSafeArea()
         }
+        .frame(height: 300)
     }
 }
 
@@ -44,11 +67,12 @@ struct MainCardItems_Previews: PreviewProvider {
 }
 
 extension Items {
-    static let pizzaStick = Items(name: "Pizza tyčinky", gram: 350, price: 3.8)
-    static let margharita = Items(name: "Margharita", gram: 580, price: 5.5)
-    static let sunkova = Items(name: "Šunková", gram: 640, price: 5.9)
-    static let funghi = Items(name: "Fungi", gram: 710, price: 6.2)
-    static let cardinala = Items(name: "Cardinala", gram: 770, price: 6.8)
-    static let ungarese = Items(name: "Ungarese", gram: 750, price: 7.0)
+    static let pizzaStick = Items(name: "Pizza tyčinky", gram: 350, price: 3.8, ingredients: ["cesnak,", "olivový olej"])
+    static let margharita = Items(name: "Margharita", gram: 580, price: 5.5, ingredients: ["paradajková omáčka,", "syr"])
+    static let sunkova = Items(name: "Šunková", gram: 640, price: 5.9, ingredients: ["paradajková omáčka,", "syr,", "šunka"])
+    static let funghi = Items(name: "Fungi", gram: 710, price: 6.2, ingredients: ["Ingredient 7", "Ingredient 8"])
+    static let cardinala = Items(name: "Cardinala", gram: 770, price: 6.8, ingredients: ["Ingredient 9", "Ingredient 10"])
+    static let ungarese = Items(name: "Ungarese", gram: 750, price: 7.0, ingredients: ["Ingredient 11", "Ingredient 12"])
 }
+
 
